aa374c5b7fc810e466e318b88bc8260caf2ad01f
Kalinin Mitko <kalinin.mitko@gmail.com>
2021-01-16 10:28:00 +0200

    Integrated native base

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,13 +1,63 @@
-import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { Col, Row, Grid } from "react-native-easy-grid";
+import {
+  Container,
+  Content,
+  Button,
+  StyleProvider,
+  Item,
+  Label,
+  Input,
+  Text,
+} from "native-base";
+import getTheme from "../theme/components";
+import platform from "../theme/variables/platform";
 
 export default function LoginScreen() {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <StyleProvider style={getTheme(platform)}>
+      <Container>
+        <Content padder enableOnAndroid>
+          <Grid>
+            <Row>
+              <Col>
+                <Item stackedLabel>
+                  <Label>Email</Label>
+                  <Input keyboardType="email-address" />
+                </Item>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Item stackedLabel>
+                  <Label>Password</Label>
+                  <Input secureTextEntry={true} />
+                </Item>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Button onPress={() => {}} block>
+                  <Text> Sign In</Text>
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Button dark transparent onPress={() => {}} block>
+                  <Text>Forget Your Password?</Text>
+                </Button>
+              </Col>
+            </Row>
+          </Grid>
+        </Content>
+        <Button dark transparent onPress={() => {}} full>
+          <Text> Don't have an account? Sign Up</Text>
+        </Button>
+      </Container>
+    </StyleProvider>
   );
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
